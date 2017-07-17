6656e84030849453121332fe7e30cedc6fab5846
Hong-She Liang <starofrainnight@gmail.com>
2017-07-17 21:07:48 +0800

    Fixed license will inserted before encoding comment line

--- srclicenser/licensers.py
+++ srclicenser/licensers.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
 
+#
+# Copyright 2017, Hong-She Liang <starofrainnight@gmail.com>.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
 '''
 A series licensers use for handle how to insert or replace license
 '''
@@ -75,6 +92,10 @@ class Licenser(object):
                     break
 
             index += 1
+
+        # If there have only have one line comment
+        if (begin_index >= 0) and (end_index < 0) and (len(self._source) > 0):
+            end_index = len(self._source)
 
         if (begin_index >= 0) and (end_index >= 0):
             return (begin_index, end_index)
